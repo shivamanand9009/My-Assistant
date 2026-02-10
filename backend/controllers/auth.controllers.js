6ae467e63a0ae7a831eb57c5af01bdbed3b0126d
Shivam Anand <78689810+shivamanand9009@users.noreply.github.com>
2026-02-11 00:28:35 +0530

    Modify cookie settings for authentication
    Updated cookie settings to use 'sameSite: none' and 'secure: true' for better security.

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -28,8 +28,8 @@ export const signUp = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      sameSite: "strict",
-      secure: false,
+      sameSite: "none",
+      secure: true,
     });
 
     return res.status(201).json(user);
@@ -57,8 +57,8 @@ export const Login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      sameSite: "strict",
-      secure: false,
+      sameSite: "none",
+      secure: true,
     });
 
     return res.status(200).json(user);
